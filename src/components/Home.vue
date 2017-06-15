2da6137ecae1be644de82b5fec7c3f91e1ebd2cb
Septian A Tama <sfirst.tama@gmail.com>
2017-06-15 10:25:15 +0700

    fix me pleasseee

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+    <h1>Home</h1>
+    <md-card v-for="cat in latestCat" :key="cat['.key']">
+      <md-card-media>
+        <img :src="cat.url" :alt="cat.comment">
+      </md-card-media>
+
+      <md-card-header>
+        <div class="md-title">{{ cat.comment }}</div>
+      </md-card-header>
+
+      <md-card-actions>
+        <router-link :to="'/detail/'+cat['.key']">
+          <md-button>Details</md-button>
+        </router-link>
+      </md-card-actions>
+    </md-card>
+</template>
+
+<script>
+export default {
+  firebase() {
+    return {
+      cats: this.$db.ref('cats'),
+    }
+  },
+  computed: {
+    latestCats: this.cats.reverse(),
+  }
+}
+</script>
+
+<style lang="css">
+.md-card {
+  margin-bottom: 16px;
+}
+</style>
