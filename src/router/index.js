fae6168f3a3fa403a5934ff2b994049d3e730248
Oscar Hermawan <email": null>
2017-06-15 11:27:27 +0700

    component-detail

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Detail from '@/components/Detail'
 import Post from '@/components/Post'
 
 Vue.use(Router)
